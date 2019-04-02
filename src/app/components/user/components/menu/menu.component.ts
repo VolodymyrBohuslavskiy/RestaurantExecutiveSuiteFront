998386a3eb3620a8dd5d20d1359b8643cc507b2f
VolodymyrBohuslavskiy <936187435bv@gmail.com>
2019-04-02 22:03:05 +0300

    1

--- src/app/components/user/components/menu/menu.component.ts
+++ src/app/components/user/components/menu/menu.component.ts
@@ -10,9 +10,9 @@ import {Category} from '../../../../models/Category';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  dishess: Dish[] = [];
-  categoryes: Category[] = [];
-
+  dishess: Dish[] = [new Dish(1, 'ololololol'), new Dish(2, 'QQQ'), new Dish(3, 'Zero')];
+  categoryes: Category[] = [new Category(null, 'Test Category')];
+  sowThisCategory = false;
 
   path = 'http://127.0.0.1:8887/';
 
@@ -30,5 +30,11 @@ export class MenuComponent implements OnInit {
       console.log(form.value.surchWord);
       form.resetForm();
     }
+  }
+
+  showDishes(category: Category) {
+    this.sowThisCategory = !this.sowThisCategory;
+    console.log(category.categoryName);
+    console.log(this.sowThisCategory);
   }
 }
